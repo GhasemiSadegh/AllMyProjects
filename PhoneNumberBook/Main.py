e350cc3aaf780a1aad267824a8b05504ecb1c69d
MrNoobPy <tarjomebama@gmail.com>
2024-06-30 17:48:59 +0200

    Creating project PhoneNumberBook, modify fixed, working on ValueErrors and exceptions

--- PhoneNumberBook/Main.py
+++ PhoneNumberBook/Main.py
@@ -1,27 +1,3 @@
-# Add a Contact:
-#
-# Store a new Contact with name and phone number.
-# View Contacts:
-#
-# Display all Contacts in a list format.
-# Search for a Contact:
-#
-# Find a Contact by name or phone number.
-# Delete a Contact:
-#
-# Remove a Contact by name or phone number.
-# Update a Contact:
-#
-# Modify the details of an existing Contact.
-# Save to File:
-#
-# Save the Contact list to an external file.
-# Load from File:
-#
-# Load Contacts from an external file.
-# Exit:
-#
-# Close the application gracefully
 def save(lines):
     with open('Details.txt', 'w') as f:
         for line in lines:
@@ -40,7 +16,7 @@ def view():
     with open('Details.txt', 'r') as f:
         lines = f.readlines()
         for line in lines:
-            line = line.strip() + '\n'
+            line = line.strip()
             print(line)
 
 
@@ -53,7 +29,6 @@ def find():
                 contact = line.strip().split(': ')
                 if contact[0] == target:
                     print(line)
-                    return line
                 else:
                     print('Full name does not exist')
 
@@ -108,14 +83,14 @@ def modify():
 
 
 while True:
-    choice = input('# Choose below: \n'
+    choice = input('\n# Choose below: \n'
                    '1. Add a Contact\n'
                    '2. View Contacts \n'
                    '3. Find a Contact \n'
                    '4. Remove a Contact \n'
                    '5. Modify a Contact \n'
                    '6. Quit the app\n'
-                   'Here>>')
+                   'Here>>\n')
     if choice == '1':
         add()
     elif choice == '2':
