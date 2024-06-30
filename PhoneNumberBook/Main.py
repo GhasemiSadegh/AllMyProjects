5c69ee3de487156c5e8a9eb63daac35efac9301a
MrNoobPy <tarjomebama@gmail.com>
2024-06-30 14:07:12 +0200

    Creating project PhoneNumberBook, stuck with save file

--- PhoneNumberBook/Main.py
+++ PhoneNumberBook/Main.py
@@ -22,13 +22,17 @@
 # Exit:
 #
 # Close the application gracefully
+def save(lines):
+    with open('Details.txt', 'a') as f:
+        f.write(lines)
 
 
 def add():
     name = input('Enter a full name:')
     number = int(input('Enter a number: '))
     with open('Details.txt', 'a') as f:
-        f.write(f'{name}, {number} \n')
+        lines = f'{name}, {number} \n'
+        save(str(lines))
 
 
 def view():
@@ -41,19 +45,66 @@ def find():
     target = input('Enter the name: ')
     with open('Details.txt', 'r') as f:
         lines = f.readlines()
-        if target in lines
+        for index, line in enumerate(lines, start=1):
+            if target in line:
+                return line
+
+
+def remove():
+    target = input('Enter the name: ')
+    with open('Details.txt', 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            if target in line:
+                lines.remove(line)
+                lines = f.readlines()
+                save(lines)
+
+
+def modify():
+    selection = input('Modify a name or a number? '
+                      '1. Name\n'
+                      '2. Number\n')
+    while True:
+        if selection == '1':
+            old_name = find()
+            new_name = input('Change to? ')
+            with open('Details.txt', 'r') as f:
+                lines = f.readlines()
+                for index, line in enumerate(lines, start=1):
+                    if old_name in line:
+                        line = line.replace(old_name, new_name)
+                    f.write(line)
+
+        elif selection == '2':
+            old_num = find()
+            new_num = input('Change to? ')
+            with open('Details.txt', 'r') as f:
+                lines = f.readlines()
+                for index, line in enumerate(lines, start=1):
+                    if old_num in line:
+                        line = line.replace(old_num, new_num)
+                    f.write(line)
+
 
 while True:
-    choice = input('choose below: '
+    choice = input('# Choose below: \n'
                    '1. Add a Contact\n'
                    '2. View Contacts \n'
                    '3. Find a Contact \n'
                    '4. Remove a Contact \n'
                    '5. Modify a Contact \n'
-                   '6. Quit the app')
+                   '6. Quit the app\n'
+                   'Here>>')
     if choice == '1':
         add()
     elif choice == '2':
         view()
     elif choice == '3':
-
+        find()
+    elif choice == '4':
+        remove()
+    elif choice == '5':
+        modify()
+    elif choice == '6':
+        break
