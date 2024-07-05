70a3255556599b50612c3a20108c21c1d755b633
MrNoobPy <tarjomebama@gmail.com>
2024-07-05 14:48:09 +0200

    Learning SQLite: organizing sqlite files

--- SQLiteBasics/Practice/VeryBasicMain.py
+++ SQLiteBasics/Practice/VeryBasicMain.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-connection = sqlite3.connect('Practice/VeryBasic.db')
+connection = sqlite3.connect('VeryBasic.db')
 cursor = connection.cursor()
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS tasks (
