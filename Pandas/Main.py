767d4bc8b73b234917dd34ecb735589a4a807d53
MrNoobPy <tarjomebama@gmail.com>
2024-07-13 15:01:54 +0200

    Learning Pandas: creating more colums and tile using a dict

--- Pandas/Main.py
+++ Pandas/Main.py
@@ -1,4 +1,11 @@
 import pandas as pd
 
+
 column = ['ali', 'reza', 'mehdi']
-data = pd.DataFrame(column)
+title_column = {'num': column,
+                'height': [180, 190, 160],
+                'weight': [90, 88, 67]
+                }
+
+data = pd.DataFrame(title_column)
+print(data)
