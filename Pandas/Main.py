41ed369569bc69dd67f1d413cb71d1224780ada1
MrNoobPy <tarjomebama@gmail.com>
2024-07-13 15:23:49 +0200

    Learning Pandas: selecting a row and then one item from the row

--- Pandas/Main.py
+++ Pandas/Main.py
@@ -1,13 +1,16 @@
 import pandas as pd
 
 title_column = {
-    'num': ['ali', 'reza', 'mehdi'],
+    'name': ['ali', 'reza', 'mehdi'],
     'height': [180, 190, 160],
     'weight': [90, 88, 67]
 }
 
 data = pd.DataFrame(title_column)
-# print(data)
+print(data)
 
 select_column = data['weight'][0]
-print(select_column)
+# print(select_column)
+
+select_row = data.iloc[1]['weight']
+print(select_row)
