1c4b8069151abd8a464e5db84348cb6c2e35a393
MrNoobPy <tarjomebama@gmail.com>
2024-07-13 15:11:32 +0200

    Learning Pandas: printing one data from one column

--- Pandas/Main.py
+++ Pandas/Main.py
@@ -1,11 +1,13 @@
 import pandas as pd
 
-
-column = ['ali', 'reza', 'mehdi']
-title_column = {'num': column,
-                'height': [180, 190, 160],
-                'weight': [90, 88, 67]
-                }
+title_column = {
+    'num': ['ali', 'reza', 'mehdi'],
+    'height': [180, 190, 160],
+    'weight': [90, 88, 67]
+}
 
 data = pd.DataFrame(title_column)
-print(data)
+# print(data)
+
+select_column = data['weight'][0]
+print(select_column)
