6a210a388fb31481e77f72866a0cf37464f4dc06
MrNoobPy <tarjomebama@gmail.com>
2024-07-14 18:24:57 +0200

    QueueTicketMachine: working on serving next customer

--- QueueTicketMachine/Main.py
+++ QueueTicketMachine/Main.py
@@ -5,29 +5,38 @@ from collections import deque
     Prio for Urgent"""
 
 
-def __init__(self):
-    self.fifo_queue = deque()
-    self.lifo_queue = deque()
-    self.priority_queue = []
+class Queue:
+    def __init__(self):
+        self.fifo_queue = deque()
+        self.lifo_queue = deque()
+        self.priority_queue = []
 
+    def book_ticket(self, customer_name, customer_type):
+        if customer_type == '1':
+            self.fifo_queue.append(customer_name)
+        elif customer_type == "2":
+            self.lifo_queue.appendleft(customer_name)
+        elif customer_type == '3':
+            priority = int(input("your priority from 1 to 3 and 1 is the highest\n "
+                                 "Here: "))
+            self.priority_queue.append((customer_name, priority))
+            self.priority_queue.sort(key=lambda x: x[1], reverse=True)
 
-def book_ticket(self, customer_name, customer_type):
-    if customer_type == '1':
-        self.fifo_queue.append(customer_name)
-    if customer_type == "2":
-        self.lifo_queue.appendleft(customer_name)
-    if customer_type == '3':
-        priority = int(input("your priority from 1 to 3 and 1 is the highest\n "
-                             "Here: "))
-        self.priority_queue.append((customer_name, priority))
-        self.priority_queue.sort(key=lambda x: x[1], reverse=True)
+    def next_ticket(self):
+        if self.fifo_queue:
+            self.fifo_queue.popleft()
+        elif self.lifo_queue:
+            self.lifo_queue.popleft()
+        elif self.priority_queue:
+            self.priority_queue.pop()
 
 
 print('Welcome')
 choice = input('Select:'
-               '1. check in')
+               '1. Check in'
+               '2. Next')
 
-
+instance = Queue()
 while True:
     cs_name = input('Your name: ')
     if choice == '1':
@@ -35,5 +44,11 @@ while True:
         1. Standard Account
         2. VIP Account
         3. Diamond Account''')
-        book_ticket(cs_name, cs_type)
+        instance.book_ticket(cs_name, cs_type)
+    elif choice == "2":
+        customer, customer_type = instance.next_ticket()
+        if customer:
+            print(f"Serving {customer} from {customer_type}")
+        else:
+            print("No customers left to serve.")
 
