e3e524b70840a7b461495e945dd867671ab3730d
MrNoobPy <tarjomebama@gmail.com>
2024-07-14 16:22:27 +0200

    QueueTicketMachine: stuck with relating the menu to function where customer_type is not recognized

--- QueueTicketMachine/Main.py
+++ QueueTicketMachine/Main.py
@@ -5,38 +5,35 @@ from collections import deque
     Prio for Urgent"""
 
 
-class QueueTicketMachine:
-    def __init__(self):
-        self.fifo_queue = deque()
-        self.lifo_queue = deque()
-        self.priority_queue = []
+def __init__(self):
+    self.fifo_queue = deque()
+    self.lifo_queue = deque()
+    self.priority_queue = []
 
-    def book_ticket(self, customer_name, customer_type):
-        if customer_type == 'Standard':
-            self.fifo_queue.append(customer_name)
-        if customer_type == "VIP":
-            self.lifo_queue.appendleft(customer_name)
-        if customer_type == 'Urgent':
-            priority = int(input("your priority from 1 to 3 and 1 is the highest\n "
-                                 "Here: "))
-            self.priority_queue.append((customer_name, priority))
-            self.priority_queue.sort(key=lambda x: x[1], reverse=True)
 
-        def serve_next():
-            if self.priority_queue:
-                pass
+def book_ticket(self, customer_name, customer_type):
+    if customer_type == '1':
+        self.fifo_queue.append(customer_name)
+    if customer_type == "2":
+        self.lifo_queue.appendleft(customer_name)
+    if customer_type == '3':
+        priority = int(input("your priority from 1 to 3 and 1 is the highest\n "
+                             "Here: "))
+        self.priority_queue.append((customer_name, priority))
+        self.priority_queue.sort(key=lambda x: x[1], reverse=True)
 
 
 print('Welcome')
 choice = input('Select:'
                '1. check in')
 
-customer_name = input('Your name: ')
 
 while True:
-
+    cs_name = input('Your name: ')
     if choice == '1':
-
-        customer_type = input('''
-        1. ''')
+        cs_type = input('''
+        1. Standard Account
+        2. VIP Account
+        3. Diamond Account''')
+        book_ticket(cs_name, cs_type)
 
