c1dc32db5ab648827fd430c7556d1ade17af05a4
MrNoobPy <tarjomebama@gmail.com>
2024-07-14 13:59:54 +0200

    QueueTicketMachine: Using deque: append, append left, pop, popleft, reverse, rotate

---
+++ Practices/deque.py
@@ -0,0 +1,8 @@
+from collections import deque
+
+names = ["Alexander", "Emily", "Michael", "Sophia",
+         "Benjamin", "Olivia", "James", "Isabella", "William", "Mia"]
+my_deque = deque(names)
+my_deque.append('Ali')
+my)-
+print(my_deque)
