71710682fc9b6dec1d5ac22718c67547b8b49102
MrNoobPy <tarjomebama@gmail.com>
2024-07-14 14:33:32 +0200

    QueueTicketMachine: deque finished

--- Practices/deque.py
+++ Practices/deque.py
@@ -4,5 +4,15 @@ names = ["Alexander", "Emily", "Michael", "Sophia",
          "Benjamin", "Olivia", "James", "Isabella", "William", "Mia"]
 my_deque = deque(names)
 my_deque.append('Ali')
-my)-
+my_deque.appendleft('Feri')
+my_deque.pop()
+my_deque.popleft()
+my_deque.remove('Emily')
+my_deque.extend(['Gholi','Ghasem'])
+print(my_deque)
+my_deque.reverse()
+print(my_deque)
+my_deque.rotate() # [+1]
+print(my_deque)
+my_deque.rotate(-3)
 print(my_deque)
