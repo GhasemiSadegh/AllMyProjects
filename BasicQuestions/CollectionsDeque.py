a10a2d62433c925d3cda0b759dcfc09bff044ccc
MrNoobPy <tarjomebama@gmail.com>
2024-07-14 11:31:09 +0200

    Learning advanced data structures: Queues: reverse LIFO

--- BasicQuestions/CollectionsDeque.py
+++ BasicQuestions/CollectionsDeque.py
@@ -1,6 +1,7 @@
 import queue
 
 """"Learning Advanced Data Structures"""
+"""First In First Out, FIFO"""
 
 q = queue.Queue()
 nums = [2, 4, 6, 8, 10]
@@ -9,3 +10,10 @@ for num in nums:
 
 print(q.get())
 print(q.get())
+
+"""Last In First Out, LIFO"""
+q_reverse = queue.LifoQueue()
+for num in nums:
+    q_reverse.put(num)
+
+print(q_reverse.get())
