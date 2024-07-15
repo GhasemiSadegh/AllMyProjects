bca927d68b107d9e7b35eed5275747d9dcd1f962
MrNoobPy <tarjomebama@gmail.com>
2024-07-15 23:07:42 +0200

    FastAPI: Installing JSON formatter on chrome

--- Fast_API/main.py
+++ Fast_API/main.py
@@ -5,25 +5,4 @@ app = FastAPI()
 
 @app.get('/')
 def hello():
-    return 'zero.'
-
-
-@app.get('/greeting')
-def hello():
-    return 'Hello Dear Customer, we are not available now.'
-
-
-@app.get('/log_in')
-def hello():
-    return 'log in page'
-
-
-@app.get('/main')
-def hello():
-    return 'home page.'
-
-
-# @app.get('/hello/{name}')
-# def hello_name(name):
-#     return f'hello {name}'
-#
+    return 'the code is working.'
