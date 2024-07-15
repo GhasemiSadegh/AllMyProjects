51d83a71e3110b9109e88185659ec472617a744b
MrNoobPy <tarjomebama@gmail.com>
2024-07-15 23:44:55 +0200

    FastAPI: stuck with not found error

--- Fast_API/main.py
+++ Fast_API/main.py
@@ -3,9 +3,6 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get('/index')
-def hello1():
-    return {
-        'message': 'hello1 is working.'
-    }
-
+@app.get('/')
+def hello():
+    return 'hello world'
