08c67c8e8579360c8c6d14727644049bd611b416
MrNoobPy <tarjomebama@gmail.com>
2024-09-25 20:20:52 +0200

    Reviewing Basics

--- Practices/review.py
+++ Practices/review.py
@@ -1,6 +1,15 @@
-nums = [1, 2, 3, 4, 5]
-odd_list = []
-for num in nums:
-    if num % 2 != 0:
-        odd_list.append(num)
-print(odd_list)
+# nums = [1, 2, 3, 4, 5]
+# odd_list = []
+# for num in nums:
+#     if num % 2 != 0:
+#         odd_list.append(num)
+# print(odd_list)
+
+def is_even(num):
+    if num % 2 == 0:
+        return False
+    return True
+
+
+is_even(2)
+
